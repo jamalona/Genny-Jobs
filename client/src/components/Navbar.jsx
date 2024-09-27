6f3c71de7beafff6ff837f76ff732bbbd8bd37d2
Jamal Onanuga <jamalona17@gmail.com>
2024-09-27 17:57:53 +0100

    changes<a>tage to Link in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar">
     <div className="logo">Geniune Jobs</div>
     <ul className="nav-links">
-      <li><a href="/">Home</a></li>
-      <li><a href="/search">Search</a></li>
-      <li><a href="add_post">Post a job</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/search">Search</Link></li>
+      <li><Link to="add_post">Post a job</Link></li>
     </ul>
     <button className="login-btn">Log in</button>
   </nav>
