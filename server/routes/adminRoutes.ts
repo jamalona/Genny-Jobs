4dbe522b2a7f525bad5df12d2511cb833e6d6ed2
Fred Moore <frederic_archer@hotmail.com>
2024-10-02 11:09:53 +0100

    update

--- server/routes/adminRoutes.ts
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const adminController = require('../controllers/adminController');
-// const router = express.Router();
-
-// router.post('/login', adminController.login);  // POST: Admin login
-// router.post('/logout', adminController.logout);// POST: Admin logout
-
-// module.exports = router;
