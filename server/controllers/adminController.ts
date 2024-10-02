f1d168d9ad56d3e831a5f573c7edc43a34ccac5c
Jamal Onanuga <jamalona17@gmail.com>
2024-10-02 11:12:05 +0100

    update

--- server/controllers/adminController.ts
+++ server/controllers/adminController.ts
@@ -1,14 +1,9 @@
-// import { Request, Response } from "express";
 // const Admin = require('../models/Admin');
 // const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
 
-// interface LoginRequest {
-//   username: string;
-//   password: string;
-// }
 // // Login
-// export const login = async ( req: Request<{},{}, LoginRequest>, res: Response): Promise<unknown> => {
+// exports.login = async (req, res) => {
 //   const { username, password } = req.body;
 //   try {
 //     const admin = await Admin.findOne({ username });
@@ -20,12 +15,11 @@
 //     const token = jwt.sign({ id: admin._id }, 'secret', { expiresIn: '1h' });
 //     res.json({ token });
 //   } catch (err) {
-//     res.status(500).send((err as Error).message);
+//     res.status(500).send(err.message);
 //   }
 // };
 
 // // Logout
-// export const logout = (req: Request, res: Response): void => {
+// exports.logout = (req, res) => {
 //   res.send('Logout endpoint to clear client-side token');
 // };
-
